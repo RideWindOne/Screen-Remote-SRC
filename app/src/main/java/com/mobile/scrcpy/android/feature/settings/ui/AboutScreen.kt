f09378831c7003f2900b10c7c278a2e35ceec0c1
星冉 <Jalapeno1868@outlook.com>
2026-04-13 13:31:01 +0800

    Replace the Telegram link with the iOS group link

--- app/src/main/java/com/mobile/scrcpy/android/feature/settings/ui/AboutScreen.kt
+++ app/src/main/java/com/mobile/scrcpy/android/feature/settings/ui/AboutScreen.kt
@@ -170,12 +170,12 @@ fun AboutScreen(onBack: () -> Unit) {
                         Modifier
                             .fillMaxWidth()
                             .height(AppDimens.listItemHeight)
-                            .padding(horizontal = 16.dp),
-                    // 临时禁用 Telegram 频道功能
-                    // .clickable {
-                    //     val intent = Intent(Intent.ACTION_VIEW, AppConstants.TELEGRAM_CHANNEL.toUri())
-                    //     context.startActivity(intent)
-                    // }.padding(horizontal = 16.dp),
+                            .padding(horizontal = 16.dp)
+                            // 临时禁用 Telegram 频道功能
+                            .clickable {
+                                val intent = Intent(Intent.ACTION_VIEW, AppConstants.TELEGRAM_CHANNEL.toUri())
+                                context.startActivity(intent)
+                            }.padding(horizontal = 16.dp),
                     horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically,
                 ) {
